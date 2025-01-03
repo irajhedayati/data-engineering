ea4268c9bdf67c0458ea21c8ce51c336287012f7
Iraj Hedayati <iraj.hedayati@gmail.com>
2025-01-03 11:52:09 -0500

    Fix the format of the tutorials table

--- dataedu-fe/src/components/TutorialCell.vue
+++ dataedu-fe/src/components/TutorialCell.vue
@@ -6,19 +6,13 @@ export default {
 </script>
 
 <template>
-<div class="col-4 mt-1 mb-1">
+<div class="col-lg-4 col-md-6 col-sm-12 col-xs-12 mt-1 mb-1">
   <div class="card h-100">
     <div class="card-body">
       <div class="row-cols-1 mb-3" style="height: 10%"><h5 class="card-title">{{ title }}</h5></div>
-      <div class="row-cols-1 mb-3" style="height: 55%">
-        <p class="card-text">{{ description }}</p>
-      </div>
-      <div class="row">
-        <div class="col-4"></div>
-        <div class="col-4"></div>
-        <div class="col-4">
+      <div class="row-cols-1 mb-3" style="height: 55%"><p class="card-text">{{ description }}</p></div>
+      <div class="row-cols-lg-3 row-cols-md-3 row-cols-sm-12 row-cols-xs-12 mb-3">
           <router-link aria-current="page" class="btn btn-primary" to="{{ link }}">Content</router-link>
-        </div>
       </div>
     </div>
   </div>
--- dataedu-fe/src/views/HomeView.vue
+++ dataedu-fe/src/views/HomeView.vue
@@ -25,8 +25,6 @@
           <tutorial-cell title="Introduction" link="/tutorials/introduction" description="A brief introduction to Data Science, Data Engineering, and Business Intelligence."/>
           <tutorial-cell title="Hadoop and Zookeeper" link="/tutorials/hadoop" description="The Apache Hadoop software library is a framework that allows for the distributed processing of large data sets across clusters of computers using simple programming models."/>
           <tutorial-cell title="Hive" link="/tutorials/hive" description="The Apache Hive data warehouse software facilitates reading, writing, and managing large datasets residing in distributed storage using SQL."/>
-        </div>
-        <div class="row">
           <tutorial-cell title="Kafka and Schema Registry" link="/tutorials/kafka" description="A messaging system based on pub/sub architecture perfectly designed for streaming and message passing."/>
           <tutorial-cell title="Spark" link="/tutorials/spark" description="Spark is a unified distributed data processing platform."/>
           <tutorial-cell title="AWS" link="/tutorials/aws" description="AWS is the world’s most comprehensive and broadly adopted cloud platform, offering over 175 fully featured services from data centers globally."/>
