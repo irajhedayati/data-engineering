b07144d5d37caf87e9065a9f7ac3047c976e5524
Iraj Hedayati <iraj.hedayati@gmail.com>
2025-01-05 21:08:02 -0500

    Fix the link in the tutorial

--- dataedu-fe/src/components/TutorialCell.vue
+++ dataedu-fe/src/components/TutorialCell.vue
@@ -12,7 +12,7 @@ export default {
       <div class="row-cols-1 mb-3" style="height: 10%"><h5 class="card-title">{{ title }}</h5></div>
       <div class="row-cols-1 mb-3" style="height: 55%"><p class="card-text">{{ description }}</p></div>
       <div class="row-cols-lg-3 row-cols-md-3 row-cols-sm-12 row-cols-xs-12 mb-3">
-        <router-link aria-current="page" class="btn btn-primary" to=link>Content</router-link>
+        <router-link aria-current="page" class="btn btn-primary" :to="link">Content</router-link>
       </div>
     </div>
   </div>
