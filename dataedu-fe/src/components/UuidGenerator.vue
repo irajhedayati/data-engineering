6da9f2225ade2e8ea0fa4fe80bf7068c3710e36b
Iraj Hedayati <iraj.hedayati@gmail.com>
2025-01-07 14:01:46 -0500

    Add KSUID generator

--- dataedu-fe/src/components/UuidGenerator.vue
+++ dataedu-fe/src/components/UuidGenerator.vue
@@ -1,101 +1,70 @@
 <template>
-  <!--  v1-->
-<!--  <div class="row mb3">-->
-<!--    <div class="col-6">-->
-<!--      <div class="input-group mb-3">-->
-<!--        <span id="basic-addon1" class="input-group-text">UUID v1 (timestamp)</span>-->
-<!--        <input :value="v1" aria-label="uuid-v1" class="form-control" disabled type="text">-->
-<!--        <button id="button-addon2" class="btn btn-outline-secondary" type="button" @click="refreshUuidV1()">-->
-<!--          <i class="bi bi-arrow-clockwise"></i>-->
-<!--        </button>-->
-<!--        <button id="button-addon2" v-clipboard="v1" class="btn btn-outline-secondary" type="button">-->
-<!--          <i class="bi bi-copy"></i>-->
-<!--        </button>-->
-<!--      </div>-->
-<!--    </div>-->
-<!--  </div>-->
-  <!--  v3-->
-<!--  <div class="row mb3">-->
-<!--    <div class="col-6">-->
-<!--      <div class="input-group mb-3">-->
-<!--        <span id="basic-addon1" class="input-group-text">UUID v3 (namespace w/ MD5) </span>-->
-<!--        <input :value="v3" aria-label="uuid-v3" class="form-control" disabled type="text">-->
-<!--        <button id="button-addon2" class="btn btn-outline-secondary" type="button" @click="refreshUuidV3()">-->
-<!--          <i class="bi bi-arrow-clockwise"></i>-->
-<!--        </button>-->
-<!--        <button id="button-addon2" v-clipboard="v3" class="btn btn-outline-secondary" type="button">-->
-<!--          <i class="bi bi-copy"></i>-->
-<!--        </button>-->
-<!--      </div>-->
-<!--    </div>-->
-<!--  </div>-->
   <!--  v4-->
-  <div class="row mb3">
-    <div class="col-6">
-      <div class="input-group mb-3">
-        <span id="basic-addon1" class="input-group-text">UUID v4 (random)</span>
-        <input :value="v4" aria-label="uuid-v4" class="form-control" disabled type="text">
-        <button id="button-addon2" class="btn btn-outline-secondary" type="button" @click="refreshUuidV4()">
-          <i class="bi bi-arrow-clockwise"></i>
-        </button>
-        <button id="button-addon2" v-clipboard="v4" class="btn btn-outline-secondary" type="button">
-          <i class="bi bi-copy"></i>
-        </button>
-      </div>
+  <div class="row-cols-1 mb3">
+    <div class="input-group mb-3">
+      <span id="basic-addon1" class="input-group-text">UUID v4 (random)</span>
+      <input :value="v4Value" aria-label="uuid-v4" class="form-control" disabled type="text">
+      <button id="button-addon2" class="btn btn-outline-secondary" type="button" @click="refreshUuidV4()">
+        <i class="bi bi-arrow-clockwise"></i>
+      </button>
+      <button id="button-addon2" v-clipboard="v4Value" class="btn btn-outline-secondary" type="button">
+        <i class="bi bi-copy"></i>
+      </button>
     </div>
   </div>
-  <!--  v5-->
-<!--  <div class="row mb3">-->
-<!--    <div class="col-6">-->
-<!--      <div class="input-group mb-3">-->
-<!--        <span id="basic-addon1" class="input-group-text">UUID v5 (namespace w/ SHA-1) </span>-->
-<!--        <input :value="v5"-->
-<!--               aria-label="uuid-v4"-->
-<!--               class="form-control"-->
-<!--               disabled-->
-<!--               type="text">-->
-<!--        <button id="button-addon2" class="btn btn-outline-secondary" type="button" @click="refreshUuidV5()">-->
-<!--          <i class="bi bi-arrow-clockwise"></i>-->
-<!--        </button>-->
-<!--        <button id="button-addon2" v-clipboard="v5" class="btn btn-outline-secondary" type="button">-->
-<!--          <i class="bi bi-copy"></i>-->
-<!--        </button>-->
-<!--      </div>-->
-<!--    </div>-->
-<!--  </div>-->
+  <!--  KSUID -->
+  <div class="row-cols-1 mb3">
+    <div class="input-group mb-3 has-validation">
+      <span id="basic-addon1" class="input-group-text">KSUID w/ random payload</span>
+      <span id="basic-addon1" class="input-group-text"><i class="bi bi-at" /></span>
+      <button id="button-addon2" class="btn btn-outline-secondary" type="button" @click="setTimeToNow()">
+        Now
+      </button>
+      <input v-model="dateTimeStr" aria-label="uuid-v4" class="form-control" :class="dateTimeStrIsValid" type="text" required>
+      <input :value="ksuidValue" aria-label="ksuid" class="form-control" disabled type="text">
+      <button id="button-addon2" v-clipboard="ksuidValue" class="btn btn-outline-secondary" type="button" >
+        <i class="bi bi-copy" />
+      </button>
+      <div class="invalid-feedback">Invalid date-time; use ISO format e.g. 2025-01-07T18:43:31.144Z</div>
+    </div>
+  </div>
 </template>
 
 <script>
-import { v1 as uuidV1, v3 as uuidV3, v4 as uuidV4, v5 as uuidV5 } from 'uuid'
+import { v4 as uuidV4 } from 'uuid'
+// import KSUID from 'ksuid'
+import { generate } from 'xksuid'
 
 export default {
   data () {
     return {
-      v1: '',
-      v3: '',
-      v4: '',
-      v5: ''
+      v4Value: '',
+      dateTimeStr: new Date().toISOString()
+    }
+  },
+  computed: {
+    dateTime () {
+      return new Date(this.dateTimeStr)
+    },
+    ksuidValue () {
+      return generate(false, this.dateTime.getTime())
+    },
+    dateTimeStrIsValid () {
+      return isNaN(Date.parse(this.dateTimeStr)) ? 'is-invalid' : ''
     }
   },
   methods: {
-    refreshUuidV1 () {
-      this.v1 = uuidV1()
-    },
-    refreshUuidV3 () {
-      this.v3 = uuidV3()
-    },
     refreshUuidV4 () {
-      this.v4 = uuidV4()
+      this.v4Value = uuidV4()
     },
-    refreshUuidV5 () {
-      this.v5 = uuidV5()
+    setTimeToNow () {
+      this.dateTimeStr = new Date().toISOString()
     }
   },
   mounted () {
-    // this.refreshUuidV1()
-    // this.refreshUuidV3()
     this.refreshUuidV4()
-    // this.refreshUuidV5()
+    this.setTimeToNow()
+    this.refreshUuidV4()
   }
 }
 </script>
--- dataedu-fe/src/router/index.js
+++ dataedu-fe/src/router/index.js
@@ -1,3 +1,4 @@
+import { nextTick } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import AvroView from '@/views/AvroView.vue'
 import HomeView from '@/views/HomeView.vue'
@@ -11,20 +12,57 @@ import TutorialAWSView from '@/views/TutorialAWSView.vue'
 import ToolsView from '@/views/ToolsView.vue'
 
 const routes = [
-  { path: '/', name: 'Home', component: HomeView },
-  { path: '/json', name: 'JsonEditor', component: JsonEditorView },
-  { path: '/tutorials/introduction', name: 'TutorialIntroductionView', component: TutorialIntroductionView },
-  { path: '/tutorials/hadoop', name: 'TutorialHadoopAndZKView', component: TutorialHadoopAndZKView },
-  { path: '/tutorials/hive', name: 'TutorialHiveView', component: TutorialHiveView },
-  { path: '/tutorials/kafka', name: 'TutorialKafkaView', component: TutorialKafkaView },
-  { path: '/tutorials/spark', name: 'TutorialSparkView', component: TutorialSparkView },
-  { path: '/tutorials/aws', name: 'TutorialAWSView', component: TutorialAWSView },
-  { path: '/avro', name: 'AvroView', component: AvroView },
-  { path: '/tools', name: 'ToolsView', component: ToolsView }
+  { path: '/', name: 'Home', component: HomeView, meta: { title: 'My DE Space' } },
+  { path: '/json', name: 'JsonEditor', component: JsonEditorView, meta: { title: 'My DE Space - JSON Editor' } },
+  {
+    path: '/tutorials/introduction',
+    name: 'TutorialIntroductionView',
+    component: TutorialIntroductionView,
+    meta: { title: 'My DE Space - Tutorials - Introduction' }
+  },
+  {
+    path: '/tutorials/hadoop',
+    name: 'TutorialHadoopAndZKView',
+    component: TutorialHadoopAndZKView,
+    meta: { title: 'My DE Space - Tutorials - Hadoop' }
+  },
+  {
+    path: '/tutorials/hive',
+    name: 'TutorialHiveView',
+    component: TutorialHiveView,
+    meta: { title: 'My DE Space - Tutorials - Hive' }
+  },
+  {
+    path: '/tutorials/kafka',
+    name: 'TutorialKafkaView',
+    component: TutorialKafkaView,
+    meta: { title: 'My DE Space - Tutorials - Kafka' }
+  },
+  {
+    path: '/tutorials/spark',
+    name: 'TutorialSparkView',
+    component: TutorialSparkView,
+    meta: { title: 'My DE Space - Tutorials - Spark' }
+  },
+  {
+    path: '/tutorials/aws',
+    name: 'TutorialAWSView',
+    component: TutorialAWSView,
+    meta: { title: 'My DE Space - Tutorials - Cloud and AWS' }
+  },
+  { path: '/avro', name: 'AvroView', component: AvroView, meta: { title: 'My DE Space - Avro Tools' } },
+  { path: '/tools', name: 'ToolsView', component: ToolsView, meta: { title: 'My DE Space - DE Tools' } }
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL), routes
+})
+
+router.afterEach((to) => {
+  nextTick(() => {
+    // noinspection JSUnresolvedReference
+    document.title = to.meta.title || 'My DE Space'
+  })
 })
 
 export default router
